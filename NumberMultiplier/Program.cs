a97dd9f875be569fd2ea12e764e2ded16a406aa8
Marshall Offutt <marshalloffutt@gmail.com>
2019-03-01 18:39:47 -0600

    Finishes multiplier

--- NumberMultiplier/Program.cs
+++ NumberMultiplier/Program.cs
@@ -6,7 +6,24 @@ namespace NumberMultiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type some numbers, and separate them with commas. No spaces!");
+
+            // 1. Split the input string into a string array
+            // 2. Loop over the array of strings, and convert to numbers
+            // 3. Mutiply each value, store results in total
+
+            string input = Console.ReadLine();
+            string[] inputArray = input.Split(',');
+            int total = 1;
+
+            foreach (string val in inputArray)
+            {
+                int number = int.Parse(val);
+                total *= number;
+            }
+
+            Console.WriteLine(total);
+            Console.ReadKey();
         }
     }
 }
